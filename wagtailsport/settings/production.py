171f26766daed29737daabd3242b76be5c7f44bf
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-02-18 09:13:35 +0300

    Initial commit

---
+++ wagtailsport/settings/production.py
@@ -0,0 +1,16 @@
+from .base import *
+import os
+
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv(DJANGO_SECRET_KEY)
+
+ALLOWED_HOSTS = ['*'] 
+
+try:
+    from .local import *
+except ImportError:
+    pass
+
+
